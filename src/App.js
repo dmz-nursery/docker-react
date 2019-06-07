59c43d58a6e352f39c9aa76dd665785c9e731daf
dmz-nursery <dmitry.zutikov@gmail.com>
2019-06-07 19:46:57 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Boom Shakalaka! Updated! Updated again!
+          Another one bites the dick!
         </p>
         <a
           className="App-link"
